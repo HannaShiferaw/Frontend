26ee66a2231f5895658455fdef6943b7117a961d
Hanna Shiferaw <hannashiferaw13@gmail.com>
2025-12-19 23:07:16 +0300

    axios baseurl updated

--- src/Api/axios.jsx
+++ src/Api/axios.jsx
@@ -5,7 +5,7 @@ const axiosInstance = axios.create({
   // baseURL:"http://127.0.0.1:5001/clone-5e17d/us-central1/api"
 
   // deployed version of amazon server on render.com
-  baseURL: "http://localhost:5000",
+  baseURL: "https://amazon-api-deploy-9q7l.onrender.com",
 });
 
 export { axiosInstance };
